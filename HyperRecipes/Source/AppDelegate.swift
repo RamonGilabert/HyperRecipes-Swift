de113e589b3b53d91fc6460f0b7595c59dfb8cdd
Christoffer Winterkvist <chris@zenangst.com>
2015-02-18 20:01:33 +0100

    Organise project

---
+++ HyperRecipes/Source/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  HyperRecipes
+//
+//  Created by Christoffer Winterkvist on 18/02/15.
+//  Copyright (c) 2015 zenangst. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        return true
+    }
+}
+
---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment this line to define a global platform for your project
+# platform :ios, '6.0'
+
+target 'HyperRecipes' do
+
+end
+
+target 'HyperRecipesTests' do
+
+end
+
