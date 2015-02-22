c74392c108092ae0974b38c3d2da67aad8a33538
Christoffer Winterkvist <chris@zenangst.com>
2015-02-22 10:05:07 +0100

    Add Recipe model

---
+++ HyperRecipes/Models/Recipe.swift
@@ -0,0 +1,14 @@
+//
+//  Recipe.swift
+//  HyperRecipes
+//
+//  Created by Christoffer Winterkvist on 22/02/15.
+//  Copyright (c) 2015 zenangst. All rights reserved.
+//
+
+import UIKit
+import Realm
+
+class Recipe: RLMObject {
+
+}
