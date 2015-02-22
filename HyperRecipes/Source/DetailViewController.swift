10706fc823e702757c38ad22d4248bae07ae478f
Christoffer Winterkvist <chris@zenangst.com>
2015-02-22 22:45:40 +0100

    Add detail view controller

---
+++ HyperRecipes/Source/DetailViewController.swift
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.swift
+//  HyperRecipes
+//
+//  Created by Christoffer Winterkvist on 22/02/15.
+//  Copyright (c) 2015 zenangst. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
