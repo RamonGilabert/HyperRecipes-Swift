6722587a62c031d4d43ad825a64528ade00af3ea
Christoffer Winterkvist <chris@zenangst.com>
2015-02-23 19:18:16 +0100

    Move extension into separate folder

---
+++ HyperRecipes/Extensions/NSDictionaryExtensions.swift
@@ -0,0 +1,14 @@
+//
+//  NSDictionaryExtensions.swift
+//  HyperRecipes
+//
+//  Created by Christoffer Winterkvist on 23/02/15.
+//  Copyright (c) 2015 zenangst. All rights reserved.
+//
+
+import Foundation
+
+extension NSDictionary {
+
+
+}
